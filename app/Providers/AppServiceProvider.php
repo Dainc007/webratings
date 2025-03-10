3330205ee1aa2c71577646ef90702f81845fec59
Daniel Heinze <danielheinze96@gmail.com>
2025-03-10 19:19:20 +0100

    Pulse, Rector & Pint config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +27,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        $isProduction = $this->app->isProduction();
+
+        Model::shouldBeStrict($isProduction);
+        DB::prohibitDestructiveCommands($isProduction);
+        URL::forceScheme('https');
+
+        date_default_timezone_set(config('app.timezone'));
+
+        if(!$isProduction) {
+            Lang::handleMissingKeysUsing(function ($key, $replace, $locale, $fallback): void {
+                Log::error("Missing translation key: {$key} in locale: {$locale}");
+            });
+        }
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
+
+        Vite::prefetch(concurrency: 3)->useAggressivePrefetching();
     }
 }

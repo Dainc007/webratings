4eb67ef4ac9947b66892c82c95c5a929f8d25a1a
Daniel Heinze <danielheinze96@gmail.com>
2025-03-31 11:03:32 +0200

    zmiany jezykowe

---
+++ resources/lang/pl/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Te dane uwierzytelniające nie pasują do naszych rekordów.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Zbyt wiele prób logowania. Proszę spróbować ponownie za :seconds sekund.',
+
+];
---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Twoje hasło zostało zresetowane.',
+    'sent' => 'Wysłaliśmy Ci link do resetowania hasła.',
+    'throttled' => 'Proszę czekać przed ponowną próbą.',
+    'token' => 'Ten token resetowania hasła jest nieprawidłowy.',
+    'user' => 'Nie możemy znaleźć użytkownika z podanym adresem email.',
+
+];

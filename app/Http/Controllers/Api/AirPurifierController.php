46214f969586467483c13b1c9c7f4eb01469cafc
Daniel Heinze <danielheinze96@gmail.com>
2025-06-11 21:48:50 +0200

    #2 AirPuriffier api resource

--- app/Http/Controllers/Api/AirPurifierController.php
+++ app/Http/Controllers/Api/AirPurifierController.php
@@ -1,16 +1,67 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AirPurifierResource;
 use App\Models\AirPurifier;
+use Illuminate\Http\Request;
 
-final class AirPurifierController extends Controller
+class AirPurifierController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
     {
-        return AirPurifier::paginate();
+        $airPurifiers = AirPurifier::query();
+
+        if($request->has('all')) {
+            $airPurifiers->all();
+        }
+
+        if($request->has('paginate')) {
+            $airPurifiers->paginate();
+        }
+
+        if($request->has('max')) {
+            $airPurifiers->limit($request->get('max'));
+        }
+
+        return AirPurifierResource::collection(
+            (AirPurifier::paginate())
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AirPurifier $airPurifier): AirPurifierResource
+    {
+        return new AirPurifierResource($airPurifier);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
 
 final class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory,
+        //        HasApiTokens,
+    Notifiable;
 
     /**
      * The attributes that are mass assignable.

4d861036a05211c579b2aba8f0dbd06e9982ab8c
Dainc007 <danielheinze96@gmail.com>
2025-11-28 17:39:56 +0100

    Typ Osuszacza jako pole wyboru

---
+++ app/Enums/DehumidifierType.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum DehumidifierType: string
+{
+    case CONDENSATION = 'kondensacyjny';
+    case ADSORPTION = 'adsorpcyjny';
+    case PELTIER = 'z ogniwem Peltiera';
+    case AIR = 'powietrza';
+    case WITH_PURIFIER = 'z oczyszczaczem';
+    case WITH_IONIZER = 'z jonizatorem';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::CONDENSATION => 'Osuszacz kondensacyjny',
+            self::ADSORPTION => 'Osuszacz adsorpcyjny',
+            self::PELTIER => 'Osuszacz z ogniwem Peltiera',
+            self::AIR => 'Osuszacz powietrza',
+            self::WITH_PURIFIER => 'Osuszacz z oczyszczaczem',
+            self::WITH_IONIZER => 'Osuszacz z jonizatorem',
+        };
+    }
+
+    /**
+     * Get all dehumidifier type options for forms
+     */
+    public static function getOptions(): array
+    {
+        $options = [];
+        foreach (self::cases() as $type) {
+            $options[$type->value] = $type->getLabel();
+        }
+        return $options;
+    }
+}
+
